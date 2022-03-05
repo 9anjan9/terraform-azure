d680f5e8937affae3895634d3e4f1d3d4e4151a0
9anjan9 <anjan-kumar.gonti@capgemini.com>
2022-03-05 15:33:54 +0530

    Upload vnetsubnet.tf

--- vnet-subnet.tf
+++ vnet-subnet.tf
@@ -1,5 +1,5 @@
 resource "azurerm_virtual_network" "myterraformNetwork" {
-    name = "var.vnetname
+    name = "var.vnetname"
     address_space = ["10.0.0.0/16"]
     location = var.location
     resource_group_name = azurerm_resource_group.myterraformGroup.name
